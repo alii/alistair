8e573522b61554593eb3283464e67b0a7fa0cdf3
Alistair Smith <hi@alistair.sh>
2023-10-25 15:17:16 +0100

    fix: use local storage work on non primitives (might be worse perf, need to look into)

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 /**
  * Use a value from local storage, with a setter. Will trigger updates wherever the same key is used around your app, and even works cross tab.
@@ -54,6 +54,10 @@ export function useLocalStorage<T>(
 	);
 
 	const getOrInit = React.useCallback(() => {
+		if (typeof window === 'undefined') {
+			return stableInit.current();
+		}
+
 		const value = window.localStorage.getItem(key);
 
 		if (value === null) {
@@ -64,23 +68,25 @@ export function useLocalStorage<T>(
 		return JSON.parse(value) as T;
 	}, [key, write]);
 
-	const value = React.useSyncExternalStore(
-		notify => {
-			const listener = (event: StorageEvent) => {
-				if (event.key === key) {
-					notify();
-				}
-			};
+	const [value, setValue] = useState<T>(() => getOrInit());
 
-			window.addEventListener('storage', listener);
+	const notify = useCallback(() => {
+		setValue(getOrInit());
+	}, []);
 
-			return () => {
-				window.removeEventListener('storage', listener);
-			};
-		},
-		getOrInit,
-		init,
-	);
+	useEffect(() => {
+		const listener = (event: StorageEvent) => {
+			if (event.key === key) {
+				notify();
+			}
+		};
+
+		window.addEventListener('storage', listener);
+
+		return () => {
+			window.removeEventListener('storage', listener);
+		};
+	}, []);
 
 	const set = React.useCallback(
 		(action: React.SetStateAction<T>) => {
